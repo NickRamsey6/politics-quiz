7719199df896154ecdb65f47cfb4f149dbdbb042
Lex Wilkins and Nick Ramsey <nick@>
2019-02-18 20:46:42 -0800

    this commit creates four different form selectors with a submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,45 @@
 $(document).ready(function() {
-  debugger;
-  var height = parseInt(prompt("Please enter your height in inches"));
+  // debugger;
+  // var height = parseInt(prompt("Please enter your height in inches"));
+  //
+  // if (height >= 60) {
+  //   $('#over60').addClass("highlight");
+  // } else if (height < 60) {
+  //   $('#under60').addClass("highlight");
+  // } else {
+  //   heightChecker(height);
+  //
+  // }
+  //
+  // function heightChecker(height) {
+  //   while ($.type(height) === NaN);
+  //     var height = parseInt(prompt('Looks like you didn\'t type a number! Please enter a number'));
+  //     if (height >= 60) {
+  //       $('#over60').addClass("highlight");
+  //     } else if (height < 60) {
+  //       $('#under60').addClass("highlight");
+  //     } else {
+  //       heightChecker(height);
+  //     }
+  // }
 
-  if (height >= 60) {
-    $('#over60').addClass("highlight");
-  } else if (height < 60) {
-    $('#under60').addClass("highlight");
-  } else {
-    heightChecker(height);
 
-  }
 
-  function heightChecker(height) {
-    while ($.type(height) === NaN);
-      var height = parseInt(prompt('Looks like you didn\'t type a number! Please enter a number'));
-      if (height >= 60) {
-        $('#over60').addClass("highlight");
-      } else if (height < 60) {
-        $('#under60').addClass("highlight");
-      } else {
-        heightChecker(height);
-      }
-  }
+
+  $('form#questions').submit(function(event) {
+    var dwelling = $('#location').val();
+    var howOld = $('#age').val();
+    var stance = $('input:radio[name=stance]:checked').val();
+    var pastPres = $('#previous').val();
+
+    $('#result').append(dwelling + howOld + stance + pastPres);
+
+
+    $('#resultbox').show();
+
+    event.preventDefault();
+
+  });
+
+
 });
